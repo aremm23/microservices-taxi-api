5056970a787f8cea349a93a9a866874a2cc8ac29
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-22 19:12:43 +0300

    initial commit: init eureka service module

---
+++ eureka-service/src/main/java/com/artsem/api/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.artsem.api.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+
+}
