89578c0d309990f3dab839b210d3508fea9ca71e
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-22 19:19:01 +0300

    initial commit: init passenger service module

---
+++ rides-service/src/main/java/com/artsem/api/ridesservice/RidesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.artsem.api.ridesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidesServiceApplication.class, args);
+    }
+
+}
---
+++ rides-service/src/test/java/com/artsem/api/ridesservice/RidesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artsem.api.ridesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
