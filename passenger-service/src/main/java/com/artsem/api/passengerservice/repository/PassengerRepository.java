a4e795f47dd7fabbbd5662dd7d2a17a7f1a1ffd8
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-28 12:16:58 +0300

    feature(passenger):create passenger repository

---
+++ passenger-service/src/main/java/com/artsem/api/passengerservice/repository/PassengerRepository.java
@@ -0,0 +1,8 @@
+package com.artsem.api.passengerservice.repository;
+
+import com.artsem.api.passengerservice.model.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface PassengerRepository extends JpaRepository<Passenger, Long>, JpaSpecificationExecutor<Passenger> {
+}
