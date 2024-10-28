9b668e02ed17d44cfe66b675813496c571059eee
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-28 21:43:46 +0300

    feature(passenger): implement controller layer for Passenger entity with basic crud operations

---
+++ passenger-service/src/main/java/com/artsem/api/passengerservice/controller/PassengerController.java
@@ -0,0 +1,58 @@
+package com.artsem.api.passengerservice.controller;
+
+import com.artsem.api.passengerservice.model.dto.PassengerRequestDto;
+import com.artsem.api.passengerservice.model.dto.PassengerResponseDto;
+import com.artsem.api.passengerservice.service.PassengerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/passengers")
+@RequiredArgsConstructor
+public class PassengerController {
+
+    private final PassengerService passengerService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PassengerResponseDto> getOne(@PathVariable Long id) {
+        PassengerResponseDto passenger = passengerService.getOne(id);
+        return ResponseEntity.ok(passenger);
+    }
+
+    @GetMapping("/by-ids")
+    public ResponseEntity<List<PassengerResponseDto>> getMany(@RequestParam List<Long> ids) {
+        List<PassengerResponseDto> passengers = passengerService.getMany(ids);
+        return ResponseEntity.ok(passengers);
+    }
+
+    @PostMapping
+    public ResponseEntity<PassengerResponseDto> create(@RequestBody PassengerRequestDto passengerDto) {
+        PassengerResponseDto createdPassenger = passengerService.create(passengerDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPassenger);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<PassengerResponseDto> patch(
+            @PathVariable Long id,
+            @RequestBody PassengerRequestDto passengerDto
+    ) {
+        PassengerResponseDto updatedPassenger = passengerService.patch(id, passengerDto);
+        return ResponseEntity.ok(updatedPassenger);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        passengerService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMany(@RequestParam List<Long> ids) {
+        passengerService.deleteMany(ids);
+        return ResponseEntity.noContent().build();
+    }
+}
