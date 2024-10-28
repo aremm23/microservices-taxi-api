0cfcbc74375cbb2c4c141faf9279cd1fa92c89cf
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-28 21:51:32 +0300

    feature(passenger): implement equals and hashcode methods in Passenger entity

--- passenger-service/src/main/java/com/artsem/api/passengerservice/model/Passenger.java
+++ passenger-service/src/main/java/com/artsem/api/passengerservice/model/Passenger.java
@@ -3,6 +3,10 @@ package com.artsem.api.passengerservice.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -25,4 +29,25 @@ public class Passenger {
     @Column(name = "phone", unique = true)
     private String phone;
 
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Passenger passenger = (Passenger) o;
+        return getId() != null && Objects.equals(getId(), passenger.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
