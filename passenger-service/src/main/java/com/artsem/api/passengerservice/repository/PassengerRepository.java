3adce775cdbf43c26091bf10ab1bb4b20a96bb81
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-28 21:41:25 +0300

    feature(passenger): create repository for Passenger entity

--- passenger-service/src/main/java/com/artsem/api/passengerservice/repository/PassengerRepository.java
+++ passenger-service/src/main/java/com/artsem/api/passengerservice/repository/PassengerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface PassengerRepository extends JpaRepository<Passenger, Long>, JpaSpecificationExecutor<Passenger> {
+    boolean existsByUsernameOrPhone(String username, String phone);
 }
