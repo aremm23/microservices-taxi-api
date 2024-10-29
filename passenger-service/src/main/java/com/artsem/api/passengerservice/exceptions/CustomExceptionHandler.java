90285d55ef8fba2465a59a8834b2baa1bc3ccc80
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-29 16:41:07 +0300

    feature(passenger): modify CustomExceptionHandler

--- passenger-service/src/main/java/com/artsem/api/passengerservice/exceptions/CustomExceptionHandler.java
+++ passenger-service/src/main/java/com/artsem/api/passengerservice/exceptions/CustomExceptionHandler.java
@@ -2,10 +2,15 @@ package com.artsem.api.passengerservice.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -15,11 +20,37 @@ public class CustomExceptionHandler {
             PassengerNotFoundedException.class,
             IllegalArgumentException.class
     })
-    private ResponseEntity<ErrorResponse> handlerException(RuntimeException e) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                e.getMessage(),
+    public ResponseEntity<ErrorResponse> handlerException(RuntimeException e) {
+        return new ResponseEntity<>(
+                createErrorResponse(e.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>(
+                createErrorResponse(parseValidationExceptionInfo(ex).toString()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    private ErrorResponse createErrorResponse(String message) {
+        return new ErrorResponse(
+                message,
                 LocalDateTime.now()
         );
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
+
+    private Map<String, String> parseValidationExceptionInfo(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
 }
