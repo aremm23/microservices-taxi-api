7ee4a0c061d7086f1d622e4e2a909a9592a40ea8
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-29 21:09:15 +0300

    feature(auth): add some keycloak enums

---
+++ auth-service/src/main/java/com/artsem/api/authservice/util/KeycloakGroup.java
@@ -0,0 +1,13 @@
+package com.artsem.api.authservice.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum KeycloakGroup {
+    DRIVER("DRIVER"),
+    PASSENGER("PASSENGER"),
+    MANAGER("MANAGER");
+    private final String group;
+}
