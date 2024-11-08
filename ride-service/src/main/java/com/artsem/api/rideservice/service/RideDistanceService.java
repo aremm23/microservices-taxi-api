89a309f40592d2bac95bcf2c2de169980aedb25d
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-11-08 16:19:25 +0300

    refactor/ride: define services interfaces and extract implementation

--- ride-service/src/main/java/com/artsem/api/rideservice/service/RideDistanceService.java
+++ ride-service/src/main/java/com/artsem/api/rideservice/service/RideDistanceService.java
@@ -1,61 +1,9 @@
 package com.artsem.api.rideservice.service;
 
-import com.artsem.api.rideservice.config.MapsApiProperties;
-import com.artsem.api.rideservice.exception.InvalidResponseElementException;
-import com.artsem.api.rideservice.exception.InvalidResponseException;
 import com.artsem.api.rideservice.model.util.DistanceAndDurationValues;
-import com.artsem.api.rideservice.model.util.DistanceMatrixResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-@Service
-@RequiredArgsConstructor
-public class RideDistanceService {
+public interface RideDistanceService {
 
-    private final MapsApiProperties mapsApiProperties;
+    DistanceAndDurationValues getDistanceAndDuration(String pickUpLocation, String dropOffLocation);
 
-    private final RestTemplate restTemplate;
-
-    public static final String OK_STATUS = "OK";
-    public static final String URL_ORIGIN_PARAM = "origins";
-    public static final String URL_DESTINATION_PARAM = "destinations";
-    public static final String URL_UNITS_PARAM = "units";
-    public static final String URL_KEY_PARAM = "key";
-
-    public DistanceAndDurationValues getDistanceAndDuration(String pickUpLocation, String dropOffLocation) {
-        String url = buildUrl(pickUpLocation, dropOffLocation);
-        DistanceMatrixResponse response = restTemplate.getForObject(url, DistanceMatrixResponse.class);
-        validateResponse(response);
-        DistanceMatrixResponse.Element element = response.getRows().get(0).getElements().get(0);
-        validateElement(element);
-        return DistanceAndDurationValues.builder()
-                .distanceValue(element.getDistance().getValue())
-                .durationValue(element.getDuration().getValue())
-                .build();
-    }
-
-    private String buildUrl(String pickUpLocation, String dropOffLocation) {
-        return UriComponentsBuilder.fromHttpUrl(mapsApiProperties.getUrl())
-                .queryParam(URL_ORIGIN_PARAM, pickUpLocation)
-                .queryParam(URL_DESTINATION_PARAM, dropOffLocation)
-                .queryParam(URL_UNITS_PARAM, mapsApiProperties.getUnits())
-                .queryParam(URL_KEY_PARAM, mapsApiProperties.getKey())
-                .build()
-                .toUriString();
-    }
-
-    private void validateElement(DistanceMatrixResponse.Element element) {
-        if (!OK_STATUS.equals(element.getStatus()) || element.getDistance() == null || element.getDuration() == null) {
-            throw new InvalidResponseElementException();
-        }
-    }
-
-    private void validateResponse(DistanceMatrixResponse response) {
-        if (response == null || !OK_STATUS.equals(response.getStatus()) || response.getRows().isEmpty() ||
-                response.getRows().get(0).getElements().isEmpty()) {
-            throw new InvalidResponseException();
-        }
-    }
 }
--- ride-service/src/main/java/com/artsem/api/rideservice/service/RideLifecycleService.java
+++ ride-service/src/main/java/com/artsem/api/rideservice/service/RideLifecycleService.java
@@ -1,114 +1,21 @@
 package com.artsem.api.rideservice.service;
 
-import com.artsem.api.rideservice.exception.InvalidRideStatusException;
-import com.artsem.api.rideservice.exception.RideNotFoundException;
-import com.artsem.api.rideservice.model.Ride;
-import com.artsem.api.rideservice.model.RideStatus;
-import com.artsem.api.rideservice.model.util.UserRole;
 import com.artsem.api.rideservice.model.dto.internal.AcceptedRideDto;
 import com.artsem.api.rideservice.model.dto.internal.CancelledRideDto;
 import com.artsem.api.rideservice.model.dto.internal.CompletedRideDto;
 import com.artsem.api.rideservice.model.dto.internal.RequestedRideDto;
 import com.artsem.api.rideservice.model.dto.internal.StartedRideDto;
-import com.artsem.api.rideservice.repository.RideRepository;
-import com.artsem.api.rideservice.util.ExceptionKeys;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+public interface RideLifecycleService {
 
-@Service
-@RequiredArgsConstructor
-public class RideLifecycleService {
+    void requestRide(RequestedRideDto rideDto);
 
-    private final RideRepository rideRepository;
+    void acceptRide(AcceptedRideDto rideDto);
 
-    private final ModelMapper mapper;
+    void startRide(StartedRideDto rideDto);
 
-    public void requestRide(RequestedRideDto rideDto) {
-        Ride ride = mapper.map(rideDto, Ride.class);
-        ride.setRequestedTime(LocalDateTime.now());
-        ride.setStatusId(RideStatus.REQUESTED.getId());
-        rideRepository.save(ride);
-    }
+    void finishRide(CompletedRideDto rideDto);
 
-    public void acceptRide(AcceptedRideDto rideDto) {
-        Ride ride = findRideOrThrow(rideDto.getRideId());
-        validateRideStatus(
-                ride.getStatusId(),
-                RideStatus.REQUESTED.getId(),
-                ExceptionKeys.REQUESTED_STATUS_EXPECTED
-        );
-        setAcceptedStatusParameters(rideDto, ride);
-        rideRepository.save(ride);
-    }
+    void cancelRide(CancelledRideDto rideDto);
 
-    private void setAcceptedStatusParameters(AcceptedRideDto rideDto, Ride ride) {
-        ride.setStatusId(RideStatus.ACCEPTED.getId());
-        ride.setAcceptedTime(LocalDateTime.now());
-        ride.setDriverId(rideDto.getDriverId());
-        ride.setCarId(rideDto.getCarId());
-        ride.setCarLicensePlate(rideDto.getCarLicensePlate());
-    }
-
-    public void startRide(StartedRideDto rideDto) {
-        Ride ride = findRideOrThrow(rideDto.getRideId());
-        validateRideStatus(
-                ride.getStatusId(),
-                RideStatus.ACCEPTED.getId(),
-                ExceptionKeys.ACCEPTED_STATUS_EXPECTED
-        );
-        ride.setStatusId(RideStatus.STARTED.getId());
-        ride.setStartedTime(LocalDateTime.now());
-        rideRepository.save(ride);
-    }
-
-    public void finishRide(CompletedRideDto rideDto) {
-        Ride ride = findRideOrThrow(rideDto.getRideId());
-        validateRideStatus(
-                ride.getStatusId(),
-                RideStatus.STARTED.getId(),
-                ExceptionKeys.STARTED_STATUS_EXPECTED
-        );
-        ride.setStatusId(RideStatus.FINISHED.getId());
-        ride.setFinishedTime(LocalDateTime.now());
-        rideRepository.save(ride);
-    }
-
-    public void cancelRide(CancelledRideDto rideDto) {
-        Ride ride = findRideOrThrow(rideDto.getRideId());
-        validateRideStatusToCancel(ride);
-        ride.setStatusId(defineCancelledByStatusId(rideDto.getUserRole()));
-        ride.setCanceledTime(LocalDateTime.now());
-        rideRepository.save(ride);
-    }
-
-    private int defineCancelledByStatusId(UserRole userRole) {
-        if (userRole.equals(UserRole.DRIVER)) {
-            return RideStatus.CANCELLED_BY_DRIVER.getId();
-        } else if (userRole.equals(UserRole.PASSENGER)) {
-            return RideStatus.CANCELLED_BY_PASSENGER.getId();
-        } else throw new IllegalArgumentException("Invalid user role");
-    }
-
-    private Ride findRideOrThrow(String rideId) {
-        return rideRepository.findById(rideId).orElseThrow(RideNotFoundException::new);
-    }
-
-    private void validateRideStatus(int rideStatus, int expectedStatus, String message) {
-        if (rideStatus != expectedStatus) {
-            throw new InvalidRideStatusException(message);
-        }
-    }
-
-    private void validateRideStatusToCancel(Ride ride) {
-        if (
-                !ride.getStatusId().equals(RideStatus.REQUESTED.getId())
-                        && !ride.getStatusId().equals(RideStatus.ACCEPTED.getId())
-                        && !ride.getStatusId().equals(RideStatus.STARTED.getId())
-        ) {
-            throw new InvalidRideStatusException(ExceptionKeys.UNABLE_CANCEL_RIDE);
-        }
-    }
 }
