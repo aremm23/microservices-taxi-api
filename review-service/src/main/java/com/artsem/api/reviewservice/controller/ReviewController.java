638bf548a89cc51241b44407f4b9ed0ef5a46214
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-11-21 12:18:20 +0300

    refactor/review: extract swagger annotations from controller

--- review-service/src/main/java/com/artsem/api/reviewservice/controller/ReviewController.java
+++ review-service/src/main/java/com/artsem/api/reviewservice/controller/ReviewController.java
@@ -1,11 +1,10 @@
 package com.artsem.api.reviewservice.controller;
 
+import com.artsem.api.reviewservice.controller.api.ReviewApi;
 import com.artsem.api.reviewservice.filter.ReviewFilter;
 import com.artsem.api.reviewservice.model.dto.request.ReviewRequestDto;
 import com.artsem.api.reviewservice.model.dto.response.ReviewResponseDto;
 import com.artsem.api.reviewservice.service.ReviewService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -30,56 +29,45 @@ import java.util.List;
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
 @Tag(name = "Review Controller", description = "API for managing reviews in the review service")
-public class ReviewController {
+public class ReviewController implements ReviewApi {
 
     private final ReviewService reviewService;
 
-    @Operation(summary = "Get a list of reviews", description = "Retrieve a paginated list of reviews with optional filters")
     @GetMapping
     public ResponseEntity<PagedModel<ReviewResponseDto>> getList(
-            @Parameter(description = "Filters for review search")
             @ModelAttribute ReviewFilter filter,
-            @Parameter(description = "Pagination information")
             Pageable pageable
     ) {
         Page<ReviewResponseDto> reviewResponseDtos = reviewService.getList(filter, pageable);
         return ResponseEntity.ok(new PagedModel<>(reviewResponseDtos));
     }
 
-    @Operation(summary = "Get review details by ID", description = "Retrieve detailed information about a specific review")
     @GetMapping("/{id}")
     public ResponseEntity<ReviewResponseDto> getById(
-            @Parameter(description = "ID of the review to retrieve")
             @PathVariable Long id
     ) {
         ReviewResponseDto review = reviewService.getById(id);
         return ResponseEntity.ok(review);
     }
 
-    @Operation(summary = "Create a new review", description = "Create and save a new review in the system")
     @PostMapping
     public ResponseEntity<ReviewResponseDto> create(
-            @Parameter(description = "Details of the review to create")
             @Valid @RequestBody ReviewRequestDto reviewDto
     ) {
         ReviewResponseDto createdReview = reviewService.create(reviewDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdReview);
     }
 
-    @Operation(summary = "Delete a review by ID", description = "Remove a review from the system by its ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(
-            @Parameter(description = "ID of the review to delete")
             @PathVariable Long id
     ) {
         reviewService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
-    @Operation(summary = "Delete multiple reviews by IDs", description = "Remove multiple reviews from the system by their IDs")
     @DeleteMapping
     public ResponseEntity<Void> deleteMany(
-            @Parameter(description = "List of review IDs to delete")
             @RequestParam List<Long> ids
     ) {
         reviewService.deleteMany(ids);
---
+++ review-service/src/main/java/com/artsem/api/reviewservice/controller/api/ReviewApi.java
@@ -0,0 +1,44 @@
+package com.artsem.api.reviewservice.controller.api;
+
+import com.artsem.api.reviewservice.filter.ReviewFilter;
+import com.artsem.api.reviewservice.model.dto.request.ReviewRequestDto;
+import com.artsem.api.reviewservice.model.dto.response.ReviewResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+
+@Tag(name = "Review Controller", description = "API for managing reviews in the review service")
+public interface ReviewApi {
+
+    @Operation(summary = "Get a list of reviews", description = "Retrieve a paginated list of reviews with optional filters")
+    ResponseEntity<PagedModel<ReviewResponseDto>> getList(
+            @Parameter(description = "Filters for review search") ReviewFilter filter,
+            @Parameter(description = "Pagination information") Pageable pageable
+    );
+
+    @Operation(summary = "Get review details by ID", description = "Retrieve detailed information about a specific review")
+    ResponseEntity<ReviewResponseDto> getById(
+            @Parameter(description = "ID of the review to retrieve") Long id
+    );
+
+    @Operation(summary = "Create a new review", description = "Create and save a new review in the system")
+    ResponseEntity<ReviewResponseDto> create(
+            @Parameter(description = "Details of the review to create") ReviewRequestDto reviewDto
+    );
+
+    @Operation(summary = "Delete a review by ID", description = "Remove a review from the system by its ID")
+    ResponseEntity<Void> delete(
+            @Parameter(description = "ID of the review to delete") Long id
+    );
+
+    @Operation(summary = "Delete multiple reviews by IDs", description = "Remove multiple reviews from the system by their IDs")
+    ResponseEntity<Void> deleteMany(
+            @Parameter(description = "List of review IDs to delete") List<Long> ids
+    );
+}
