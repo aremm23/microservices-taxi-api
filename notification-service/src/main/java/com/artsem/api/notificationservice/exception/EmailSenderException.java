73e6cab406eae325f3efd8bfd314b202335aca42
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-12-02 11:17:15 +0300

    feature/notification: refactor custom exception

--- notification-service/src/main/java/com/artsem/api/notificationservice/exception/EmailSenderException.java
+++ notification-service/src/main/java/com/artsem/api/notificationservice/exception/EmailSenderException.java
@@ -1,7 +1,10 @@
 package com.artsem.api.notificationservice.exception;
 
+import com.artsem.api.notificationservice.util.ExceptionKeys;
+import jakarta.mail.MessagingException;
+
 public class EmailSenderException extends RuntimeException {
-    public EmailSenderException(Throwable cause) {
-        super("Failed to send email: %s".formatted(cause));
+    public EmailSenderException(MessagingException cause) {
+        super(ExceptionKeys.EMAIL_SENDER_EXCEPTION.formatted(cause));
     }
 }
