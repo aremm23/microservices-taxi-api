558a7f315e32a8bd06e1f0b46d12d51f0ab34398
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-12-03 13:16:03 +0300

    refactor/ride: cleanup code

--- ride-service/src/main/java/com/artsem/api/rideservice/config/RedisConfig.java
+++ ride-service/src/main/java/com/artsem/api/rideservice/config/RedisConfig.java
@@ -16,6 +16,11 @@ import java.time.Duration;
 @EnableCaching
 public class RedisConfig {
 
+    public static final String DISTANCE_AND_DURATION_CACHE_NAME = "distanceAndDurationCache";
+    public static final String PASSENGERS_ID_CACHE_NAME = "passengersIdCache";
+    public static final int DISTANCE_AND_DURATION_CACHE_TTL_SECONDS = 30;
+    public static final int PASSENGER_ID_CACHE_TTL_HOURS = 3;
+
     private static RedisSerializationContext.SerializationPair<Object> getValueSerializationPair() {
         return RedisSerializationContext.SerializationPair.fromSerializer(
                 new GenericJackson2JsonRedisSerializer()
@@ -37,14 +42,14 @@ public class RedisConfig {
     public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(cacheConfiguration())
-                .withCacheConfiguration("distanceAndDurationCache",
+                .withCacheConfiguration(DISTANCE_AND_DURATION_CACHE_NAME,
                         RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(Duration.ofSeconds(30))
+                                .entryTtl(Duration.ofSeconds(DISTANCE_AND_DURATION_CACHE_TTL_SECONDS))
                                 .serializeValuesWith(getValueSerializationPair())
                                 .serializeKeysWith(getKeySerializationPair()))
-                .withCacheConfiguration("passengersIdCache",
+                .withCacheConfiguration(PASSENGERS_ID_CACHE_NAME,
                         RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(Duration.ofHours(3))
+                                .entryTtl(Duration.ofHours(PASSENGER_ID_CACHE_TTL_HOURS))
                                 .serializeValuesWith(getValueSerializationPair())
                                 .serializeKeysWith(getKeySerializationPair()))
                 .build();
